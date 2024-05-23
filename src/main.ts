5796a18ec39823dbcaf124d47d799c83b05a8e5c
Julio Zeledón <julio@MacBook-Pro-Julio.local>
2024-05-22 19:41:08 -0600

    config cors to enable localhost origin for development

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from the Next.js frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(5000);
 }
 bootstrap();
